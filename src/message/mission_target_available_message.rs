277e709601a20318ff105be78aae4e56957ad085
Michael Watzko <michael@watzko.de>
2017-09-10 23:16:25 +0200

    Add MissionTargetAvailableMessage

---
+++ src/message/mission_target_available_message.rs
@@ -0,0 +1,103 @@
+
+use std::fmt;
+use std::fmt::Write;
+use std::sync::Arc;
+use std::sync::Weak;
+use std::sync::RwLock;
+use std::borrow::Borrow;
+use std::borrow::BorrowMut;
+
+use Team;
+use Error;
+use Player;
+use Connector;
+use UniverseGroup;
+use UniversalEnumerable;
+
+use unit::ControllableInfo;
+
+use net::Packet;
+use net::BinaryReader;
+
+use message::GameMessage;
+use message::GameMessageData;
+use message::FlattiverseMessage;
+use message::FlattiverseMessageData;
+
+impl_downcast!(MissionTargetAvailableMessage);
+pub trait MissionTargetAvailableMessage : GameMessage {
+    fn mission_target_name(&self) -> &str;
+
+    fn mission_target_team(&self) -> &Arc<RwLock<Team>>;
+}
+
+pub struct MissionTargetAvailableMessageData {
+    data:   GameMessageData,
+    name:   String,
+    team:   Arc<RwLock<Team>>,
+}
+
+impl MissionTargetAvailableMessageData {
+    pub fn from_packet(connector: &Arc<Connector>, packet: &Packet, reader: &mut BinaryReader) -> Result<MissionTargetAvailableMessageData, Error> {
+        Ok(MissionTargetAvailableMessageData {
+            data:   GameMessageData::from_packet(connector, packet, reader)?,
+            team:   {
+                let id = reader.read_unsigned_byte()?;
+                let player = connector.player().upgrade();
+                let player = player.ok_or(Error::PlayerNotAvailable)?;
+                let player = player.read()?;
+                let group  = player.universe_group().upgrade();
+                let group  = group.ok_or(Error::PlayerNotInUniverseGroup)?;
+                let group  = group.read()?;
+                group.team(id).clone().ok_or(Error::TeamNotAvailable)?
+            },
+            name:   reader.read_string()?,
+        })
+    }
+}
+
+impl Borrow<GameMessageData> for MissionTargetAvailableMessageData {
+    fn borrow(&self) -> &GameMessageData {
+        &self.data
+    }
+}
+impl BorrowMut<GameMessageData> for MissionTargetAvailableMessageData {
+    fn borrow_mut(&mut self) -> &mut GameMessageData {
+        &mut self.data
+    }
+}
+impl Borrow<FlattiverseMessageData> for MissionTargetAvailableMessageData {
+    fn borrow(&self) -> &FlattiverseMessageData {
+        (self.borrow() as &GameMessageData).borrow()
+    }
+}
+impl BorrowMut<FlattiverseMessageData> for MissionTargetAvailableMessageData {
+    fn borrow_mut(&mut self) -> &mut FlattiverseMessageData {
+        (self.borrow_mut() as &mut GameMessageData).borrow_mut()
+    }
+}
+
+
+impl<T: 'static + Borrow<MissionTargetAvailableMessageData> + BorrowMut<MissionTargetAvailableMessageData> + GameMessage> MissionTargetAvailableMessage for T {
+
+    fn mission_target_name(&self) -> &str {
+        &self.borrow().name
+    }
+
+    fn mission_target_team(&self) -> &Arc<RwLock<Team>> {
+        &self.borrow().team
+    }
+}
+
+impl fmt::Display for MissionTargetAvailableMessageData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}] MissionTarget \"{}\" of Team {} is available again.",
+            (self as &FlattiverseMessage).timestamp(),
+            self.name,
+            match self.team.read() {
+                Err(_) => "",
+                Ok(ref read) => read.name()
+            },
+        )
+    }
+}
--- src/message/mod.rs
+++ src/message/mod.rs
@@ -11,6 +11,7 @@ mod player_unit_reset_message;
 mod player_unit_deceased_message;
 mod player_unit_continued_message;
 mod player_unit_logged_off_message;
+mod mission_target_available_message;
 mod player_unit_shot_by_unit_message;
 mod player_unit_shot_by_player_message;
 mod player_unit_hit_own_target_message;
@@ -33,6 +34,7 @@ pub use self::player_unit_reset_message::*;
 pub use self::player_unit_deceased_message::*;
 pub use self::player_unit_continued_message::*;
 pub use self::player_unit_logged_off_message::*;
+pub use self::mission_target_available_message::*;
 pub use self::player_unit_shot_by_unit_message::*;
 pub use self::player_unit_shot_by_player_message::*;
 pub use self::player_unit_hit_own_target_message::*;
@@ -114,6 +116,7 @@ pub fn from_reader(connector: &Arc<Connector>, packet: &Packet) -> Result<Box<Fl
         0x30 => Ok(Box::new(PlayerUnitHitMissionTargetMessageData       ::from_packet(connector, packet, reader)?)),
         0x31 => Ok(Box::new(PlayerUnitHitOwnTargetMessageData           ::from_packet(connector, packet, reader)?)),
         0x32 => Ok(Box::new(PlayerUnitHitEnemyTargetMessageData         ::from_packet(connector, packet, reader)?)),
+        0x33 => Ok(Box::new(MissionTargetAvailableMessageData           ::from_packet(connector, packet, reader)?)),
         _ => Err(Error::UnknownMessageType)
     }
 }
