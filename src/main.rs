08258a271e9d8575905a85bab3d77618ff91907f
Michael Watzko <michael@watzko.de>
2019-10-29 21:54:48 +0100

    Remove unused code

--- src/main.rs
+++ src/main.rs
@@ -117,23 +117,6 @@ async fn main() {
             }
         }
     }
-
-    // for _ in 0..100 {
-        // sleep(Duration::from_millis(100));
-        //connection.send(Packet::default()).await.unwrap();
-        //connection.send(Packet::new_oob()).await.unwrap();
-        //connection.flush().await.unwrap();
-    //}
-
-    /*
-                self.universes
-                    .iter()
-                    .flat_map(|u| u.as_ref())
-                    .for_each(|u| info!("Universe: {:#?}", u));
-                    */
-
-
-    println!("async");
 }
 
 pub fn init_logger(package: &str, level: Option<LevelFilter>) -> Result<::log4rs::Handle, SetLoggerError> {
