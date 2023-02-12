3abf43a47a29f03eeb3809c4422a772d30710b2d
Michael Watzko <michael@watzko.de>
2023-02-12 13:49:52 +0100

    Update PlayerUnit with new properties

--- src/units/player_unit.rs
+++ src/units/player_unit.rs
@@ -7,6 +7,18 @@ pub struct PlayerUnit {
     // pub controllable: ControllableId,
     #[serde(rename = "turnRate")]
     pub turn_rate: f64,
+    #[serde(rename = "requestedScanDirection")]
+    pub requested_scan_direction: f64,
+    #[serde(rename = "requestedScanWidth")]
+    pub requested_scan_width: f64,
+    #[serde(rename = "requestedScanRange")]
+    pub requested_scan_range: f64,
+    #[serde(rename = "scanDirection")]
+    pub scan_direction: f64,
+    #[serde(rename = "scanWidth")]
+    pub scan_width: f64,
+    #[serde(rename = "scanRange")]
+    pub scan_range: f64,
     // #[serde(default)]
     // pub systems: PlayerUnitSystems,
 }
