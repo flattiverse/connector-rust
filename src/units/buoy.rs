76f296c62447fb968b91fcb5361b59868d13090c
Michael Watzko <michael@watzko.de>
2023-02-16 08:49:59 +0100

    Update Buoy (add hints and kind)

--- src/units/buoy.rs
+++ src/units/buoy.rs
@@ -1,4 +1,5 @@
 use crate::units::orbits::Orbit;
+use crate::vector::Vector;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -6,4 +7,18 @@ pub struct Buoy {
     #[serde(default)]
     pub orbits: Vec<Orbit>,
     pub message: String,
+    #[serde(default)]
+    pub hints: Vec<Vector>,
+    #[sere(rename = "messageKind")]
+    pub kind: MessageKind,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum MessageKind {
+    #[serde(rename = "normal")]
+    Normal,
+    #[serde(rename = "warning")]
+    Warning,
+    #[serde(rename = "danger")]
+    Danger,
 }
