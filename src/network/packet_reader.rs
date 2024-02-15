107b088586569e3b65ef1656b8bb14eba396efc3
Michael Watzko <michael@watzko.de>
2024-02-16 00:34:45 +0100

    Add full PacketReader and PacketWriter impl

--- src/network/packet_reader.rs
+++ src/network/packet_reader.rs
@@ -1,20 +1,126 @@
-use crate::network::PacketHeader;
-use bytes::{Buf, BufMut, BytesMut};
+use bytes::{Buf, BytesMut};
 
 pub trait PacketReader {
+    fn read_sbyte(&mut self) -> i8;
+    fn read_byte(&mut self) -> u8;
+    fn read_int16(&mut self) -> i16;
+    fn read_uint16(&mut self) -> u16;
     fn read_int32(&mut self) -> i32;
-    fn read_string(&mut self, length: usize) -> String;
+    fn read_uint32(&mut self) -> u32;
+    fn read_int64(&mut self) -> i64;
+    fn read_uint64(&mut self) -> u64;
+    fn read_1s(&mut self, shift: f64) -> f64;
+    fn read_1u(&mut self, shift: f64) -> f64;
+    fn read_2s(&mut self, shift: f64) -> f64;
+    fn read_2u(&mut self, shift: f64) -> f64;
+    fn read_4s(&mut self, shift: f64) -> f64;
+    fn read_4u(&mut self, shift: f64) -> f64;
+    fn read_8s(&mut self, shift: f64) -> f64;
+    fn read_8u(&mut self, shift: f64) -> f64;
+    fn read_boolean(&mut self) -> bool;
+    fn read_string(&mut self) -> String;
+    fn read_nullable_byte(&mut self) -> Option<u8>;
 }
 
 impl PacketReader for BytesMut {
+    #[inline]
+    fn read_sbyte(&mut self) -> i8 {
+        self.get_i8()
+    }
+
+    #[inline]
+    fn read_byte(&mut self) -> u8 {
+        self.get_u8()
+    }
+
+    #[inline]
+    fn read_int16(&mut self) -> i16 {
+        self.get_i16_le()
+    }
+
+    #[inline]
+    fn read_uint16(&mut self) -> u16 {
+        self.get_u16_le()
+    }
+
     #[inline]
     fn read_int32(&mut self) -> i32 {
         self.get_i32_le()
     }
 
-    fn read_string(&mut self, length: usize) -> String {
-        let value = String::from_utf8(self[..length].to_vec());
+    #[inline]
+    fn read_uint32(&mut self) -> u32 {
+        self.get_u32_le()
+    }
+
+    #[inline]
+    fn read_int64(&mut self) -> i64 {
+        self.get_i64_le()
+    }
+
+    #[inline]
+    fn read_uint64(&mut self) -> u64 {
+        self.get_u64_le()
+    }
+
+    #[inline]
+    fn read_1s(&mut self, shift: f64) -> f64 {
+        f64::from(self.read_sbyte()) / shift
+    }
+
+    #[inline]
+    fn read_1u(&mut self, shift: f64) -> f64 {
+        f64::from(self.read_byte()) / shift
+    }
+
+    #[inline]
+    fn read_2s(&mut self, shift: f64) -> f64 {
+        f64::from(self.read_int16()) / shift
+    }
+
+    #[inline]
+    fn read_2u(&mut self, shift: f64) -> f64 {
+        f64::from(self.read_uint16()) / shift
+    }
+
+    #[inline]
+    fn read_4s(&mut self, shift: f64) -> f64 {
+        f64::from(self.read_int32()) / shift
+    }
+
+    #[inline]
+    fn read_4u(&mut self, shift: f64) -> f64 {
+        f64::from(self.read_uint32()) / shift
+    }
+
+    #[inline]
+    fn read_8s(&mut self, shift: f64) -> f64 {
+        self.read_int64() as f64 / shift
+    }
+
+    #[inline]
+    fn read_8u(&mut self, shift: f64) -> f64 {
+        self.read_uint64() as f64 / shift
+    }
+
+    #[inline]
+    fn read_boolean(&mut self) -> bool {
+        self.read_sbyte() == 1
+    }
+
+    fn read_string(&mut self) -> String {
+        let length = self.read_int16();
+        let length = length as usize;
+        let string = String::from_utf8((&self[..length]).to_vec());
         self.advance(length);
-        value.expect("Invalid UTF-8 Characters received")
+        string.expect("Invalid UTF-8 received")
+    }
+
+    fn read_nullable_byte(&mut self) -> Option<u8> {
+        if self.read_boolean() {
+            Some(self.read_byte())
+        } else {
+            None
+        }
     }
 }
--- src/network/packet_writer.rs
+++ src/network/packet_writer.rs
@@ -1,14 +1,126 @@
 use bytes::{BufMut, BytesMut};
-use std::io::Write;
 
 pub trait PacketWriter {
+    fn write_sbyte(&mut self, number: i8);
+    fn write_byte(&mut self, number: u8);
+    fn write_int16(&mut self, number: i16);
+    fn write_uint16(&mut self, number: u16);
+    fn write_int32(&mut self, number: i32);
+    fn write_uint32(&mut self, number: u32);
+    fn write_int64(&mut self, number: i64);
+    fn write_uint64(&mut self, number: u64);
+    fn write_1s(&mut self, number: f64, shift: f64);
+    fn write_1u(&mut self, number: f64, shift: f64);
+    fn write_2s(&mut self, number: f64, shift: f64);
+    fn write_2u(&mut self, number: f64, shift: f64);
     fn write_4s(&mut self, number: f64, shift: f64);
+    fn write_4u(&mut self, number: f64, shift: f64);
+    fn write_8s(&mut self, number: f64, shift: f64);
+    fn write_8u(&mut self, number: f64, shift: f64);
+    fn write_boolean(&mut self, value: bool);
+    fn write_string(&mut self, text: &str);
+    fn write_nullable_byte(&mut self, value: Option<u8>);
 }
 
 impl PacketWriter for BytesMut {
+    #[inline]
+    fn write_sbyte(&mut self, number: i8) {
+        self.put_i8(number);
+    }
+
+    #[inline]
+    fn write_byte(&mut self, number: u8) {
+        self.put_u8(number);
+    }
+
+    #[inline]
+    fn write_int16(&mut self, number: i16) {
+        self.put_i16_le(number);
+    }
+
+    #[inline]
+    fn write_uint16(&mut self, number: u16) {
+        self.put_u16_le(number);
+    }
+
+    #[inline]
+    fn write_int32(&mut self, number: i32) {
+        self.put_i32_le(number);
+    }
+
+    #[inline]
+    fn write_uint32(&mut self, number: u32) {
+        self.put_u32_le(number);
+    }
+
+    #[inline]
+    fn write_int64(&mut self, number: i64) {
+        self.put_i64_le(number);
+    }
+
+    #[inline]
+    fn write_uint64(&mut self, number: u64) {
+        self.put_u64_le(number);
+    }
+
+    fn write_1s(&mut self, number: f64, shift: f64) {
+        let value = number * shift + 0.5;
+        self.put_i8(value as _);
+    }
+
+    fn write_1u(&mut self, number: f64, shift: f64) {
+        let value = number * shift + 0.5;
+        self.put_u8(value as _);
+    }
+
+    fn write_2s(&mut self, number: f64, shift: f64) {
+        let value = number * shift + 0.5;
+        self.put_i16_le(value as _);
+    }
+
+    fn write_2u(&mut self, number: f64, shift: f64) {
+        let value = number * shift + 0.5;
+        self.put_u16_le(value as _);
+    }
+
     fn write_4s(&mut self, number: f64, shift: f64) {
         let value = number * shift + 0.5;
-        let value = value as i32;
-        self.put_i32_le(value);
+        self.put_i32_le(value as _);
+    }
+
+    fn write_4u(&mut self, number: f64, shift: f64) {
+        let value = number * shift + 0.5;
+        self.put_u32_le(value as _);
+    }
+
+    fn write_8s(&mut self, number: f64, shift: f64) {
+        let value = number * shift + 0.5;
+        self.put_i64_le(value as _);
+    }
+
+    fn write_8u(&mut self, number: f64, shift: f64) {
+        let value = number * shift + 0.5;
+        self.put_u64_le(value as _);
+    }
+
+    #[inline]
+    fn write_boolean(&mut self, value: bool) {
+        self.put_u8(if value { 1 } else { 0 })
+    }
+
+    fn write_string(&mut self, text: &str) {
+        let bytes = text.as_bytes();
+        self.write_int16(bytes.len() as _);
+        self.put_slice(bytes);
+    }
+
+    fn write_nullable_byte(&mut self, value: Option<u8>) {
+        match value {
+            Some(value) => {
+                self.write_boolean(true);
+                self.write_byte(value);
+            }
+            None => self.write_boolean(false),
+        }
     }
 }
