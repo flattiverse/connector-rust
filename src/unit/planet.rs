ec9e738db49ce871d663009f173a35c608b5367a
Michael Watzko <michael@watzko.de>
2024-02-18 00:49:33 +0100

    Fix Planet, Moon and Meteoroid not being Harvestable

--- src/unit/planet.rs
+++ src/unit/planet.rs
@@ -1,22 +1,30 @@
 use crate::hierarchy::ClusterId;
 use crate::network::PacketReader;
-use crate::unit::{CelestialBody, Unit, UnitKind};
+use crate::unit::sub_components::HarvestableSection;
+use crate::unit::{CelestialBody, Harvestable, Unit, UnitKind};
 use crate::{NamedUnit, Vector};
 
 #[derive(Debug)]
 pub struct Planet {
     body: CelestialBody,
+    harvestable: Harvestable,
 }
 
 impl Planet {
     pub fn new(cluster: ClusterId, reader: &mut dyn PacketReader) -> Self {
         Self {
             body: CelestialBody::new(cluster, reader),
+            harvestable: Harvestable::new(reader),
         }
     }
 
     // TODO pub async fn configure
     // TODO pub async fn remove
+
+    #[inline]
+    pub fn harvestable_sections(&self) -> &[HarvestableSection] {
+        &self.harvestable.sections
+    }
 }
 
 impl NamedUnit for Planet {
