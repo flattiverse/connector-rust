ef887e1613753bce708084e0700b692a8452cca5
Michael Watzko <michael@watzko.de>
2024-02-27 11:37:34 +0100

    Remove comment

--- src/hierarchy/cluster.rs
+++ src/hierarchy/cluster.rs
@@ -158,7 +158,6 @@ impl Cluster {
         kind: UnitKind,
         reader: &mut dyn PacketReader,
     ) -> Result<FlattiverseEvent, GameError> {
-        // let unit_kind = UnitKind::try_from_primitive(packet.header().param0()).unwrap();
         let unit = crate::unit::from_packet(self.id, kind, reader, self.connection.clone())?;
         let name = unit.name().to_string();
         self.units.insert(name.clone(), unit);
