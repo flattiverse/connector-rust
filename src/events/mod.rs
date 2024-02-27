0fdc4c2dba804931dc123778e7e14eb84b40c9d6
Michael Watzko <michael@watzko.de>
2024-02-27 09:33:41 +0100

    Fix docs for FlattiveseEvent

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -8,12 +8,12 @@ pub enum FlattiverseEvent {
     PingMeasured(Duration),
     /// The [`crate::hierarchy::Cluster`] of the given id was updated.
     GalaxyUpdated(GalaxyId),
-    /// The [`crate::Cluster`] of the given [`crate::Galaxy`] was updated.
+    /// The [`crate::Cluster`] of the given [`crate::hierarchy::Galaxy`] was updated.
     ClusterUpdated {
         galaxy: GalaxyId,
         cluster: ClusterId,
     },
-    /// The [`crate::Cluster`] of the given [`crate::Galaxy`] was removed.
+    /// The [`crate::Cluster`] of the given [`crate::hierarchy::Galaxy`] was removed.
     ClusterRemoved {
         galaxy: GalaxyId,
         cluster: ClusterId,
@@ -30,34 +30,34 @@ pub enum FlattiverseEvent {
         cluster: ClusterId,
         region: RegionId,
     },
-    /// The [`crate::Team`] of the given [`crate::Galaxy`] was updated.
+    /// The [`crate::Team`] of the given [`crate::hierarchy::Galaxy`] was updated.
     TeamUpdated {
         galaxy: GalaxyId,
         team: TeamId,
     },
-    /// The [`crate::Team`] of the given [`crate::Galaxy`] was removed.
+    /// The [`crate::Team`] of the given [`crate::hierarchy::Galaxy`] was removed.
     TeamRemoved {
         galaxy: GalaxyId,
         team: TeamId,
     },
-    /// The [`crate::hierarchy::ShipDesign`] of the given [`crate::Galaxy`] was updated.
+    /// The [`crate::hierarchy::ShipDesign`] of the given [`crate::hierarchy::Galaxy`] was updated.
     ShipDesignUpdated {
         galaxy: GalaxyId,
         ship_design: ShipDesignId,
     },
-    /// The [`crate::Upgrade`] of the given [`crate::unit::ShipDesign`] in the given
-    /// [`crate::hierarchy::Galaxy`] was upated.
+    /// The [`crate::hierarchy::ShipUpgrade`] of the given [`crate::hierarchy::ShipDesign`] in the
+    /// given [`crate::hierarchy::Galaxy`] was upated.
     UpgradeUpdated {
         galaxy: GalaxyId,
         ship: ShipDesignId,
         upgrade: ShipUpgradeId,
     },
-    /// The [`crate::Player`] of the given [`crate::Galaxy`] was updated.
+    /// The [`crate::Player`] of the given [`crate::hierarchy::Galaxy`] was updated.
     PlayerUpdated {
         galaxy: GalaxyId,
         player: PlayerId,
     },
-    /// The [`crate::Player`] of the given [`crate::Galaxy`] was removed.
+    /// The [`crate::Player`] of the given [`crate::hierarchy::Galaxy`] was removed.
     PlayerRemoved {
         galaxy: GalaxyId,
         player: PlayerId,
@@ -95,12 +95,12 @@ pub enum FlattiverseEvent {
         controllable_info: ControllableInfoId,
     },
 
-    /// The [`crate::Controllable`] for the given values was updated.
+    /// The [`crate::controllable::Controllable`] for the given values was updated.
     ControllableUpdated {
         galaxy: GalaxyId,
         controllable: ControllableId,
     },
-    /// The [`crate::Controllable`] for the given values was removed.
+    /// The [`crate::controllable::Controllable`] for the given values was removed.
     ControllableRemoved {
         galaxy: GalaxyId,
         controllable: ControllableId,
